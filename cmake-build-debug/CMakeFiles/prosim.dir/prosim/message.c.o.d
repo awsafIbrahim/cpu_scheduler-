898094bde80faac558010442a995551c1bc1d265
Awsaf Ibrahim <aw613718@dal.ca>
2025-08-24 22:42:14 -0300

    Deleted build time artifacts and added gitignore

--- cmake-build-debug/CMakeFiles/prosim.dir/prosim/message.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/prosim.dir/prosim/message.c.o: \
- /users/cs/aibrahim/users/cs/aibrahim/prosim/message.c \
- /usr/include/stdc-predef.h
--- cmake-build-debug/CMakeFiles/prosim.dir/prosim/process.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/prosim.dir/prosim/process.c.o: \
- /users/cs/aibrahim/users/cs/aibrahim/prosim/process.c \
- /usr/include/stdc-predef.h
